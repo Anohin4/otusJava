3fe50309bd89c03b0a035f634c7810a05624805a
Anokhin Evgeniy <enanohin@mts.ru>
2023-01-25 08:21:30 +0300

    add hw-02

---
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -0,0 +1,18 @@
+package homework;
+
+
+import java.util.Stack;
+
+public class CustomerReverseOrder {
+
+    Stack<Customer> stack = new Stack<>();
+
+    public void add(Customer customer) {
+        stack.add(customer);
+
+    }
+
+    public Customer take() {
+        return stack.pop();
+    }
+}
