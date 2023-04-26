f2b51328850ee0158b0c979a14c0e6c3aa43b471
Anokhin Evgeniy <enanohin@mts.ru>
2023-04-26 09:03:07 +0300

    small fix

--- hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -30,7 +30,7 @@ public class Phone implements Cloneable {
     private Long phoneId;
     private String number;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "client_id")
     private Client client;
 
